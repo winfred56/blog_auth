b3867e4457587d7a45b4673cae511e8677e7ccc8
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-01-21 12:58:25 +0000

    1st migration

--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
-from . import models
+from .models import Post
+
+
+def index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog_posts/index.html',context)
+
+
+#def detail(request, id):
