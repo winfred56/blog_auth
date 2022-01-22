143f19e9304140349ebf56bed5996c6c80f91bb9
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-01-22 13:50:27 +0000

    views #comments

--- blog_posts/urls.py
+++ blog_posts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('posts/<int:id>/', views.detail, name='detail'),
     path('posts/update/<int:id>/', views.update, name='update'),
     path('posts/delete/<int:id>/', views.delete, name='delete'),
-    path('posts/comment/<int:id>/', views.comments, name='comment'),
-    path('posts/comment/update/<int:id>/', views.update_comment, name='update_comment'),
+    #path('posts/comment/<int:id>/', views.comments, name='comment'),
+    path('posts/comment/<int:id>/update/', views.update_comment, name='update_comment'),
     path('posts/comment/delete/<int:id>/', views.delete_comment, name='delete_comment'),
 ]
--- blog_posts/views.py
+++ blog_posts/views.py
@@ -13,8 +13,27 @@ def index(request):
 
 def detail(request, id):
     post = Post.objects.get(id=id)
+    comments = post.comments.all()
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            #sets the comment author to the current logged in user
+            comment_form.instance.author = request.user
+            #Gets the comment but doesn't save in the database yet
+            new_comment = comment_form.save(commit=False)
+            #assigns the comment to the current post id and saves it
+            new_comment.post = post
+            new_comment.save()
+            #Empty the form
+            comment_form  = CommentForm()
+
+    else:
+        comment_form = CommentForm()
     context = {
-        'post':post
+        'post':post,
+        'form': comment_form,
+        'comments': comments
     }
     return render(request, 'blog_posts/detail.html', context)
 
@@ -22,9 +41,11 @@ def create(request):
     if request.method == 'POST':
         form = CreatePostForm(request.POST)
         if form.is_valid():
+            #Assigns the author of the post to the current logged in user
             form.instance.author = request.user
             form.save()
-            return redirect('index')
+            #returns to the post's detail
+            return redirect('f/posts/{post.id}')
     else:
         form = CreatePostForm()
     context = {
@@ -37,7 +58,7 @@ def update(request,id):
     form = UpdatePostForm(request.POST or None, instance = post)
     if form.is_valid():
        form.save()
-       return HttpResponseRedirect("/")
+       return redirect(f'/posts/{post.id}') #redirects to post detail view
     context = {
         'form':form,
         'post':post
@@ -51,42 +72,25 @@ def delete(request,id):
         return HttpResponseRedirect("/")
     return render(request, 'blog_posts/delete.html', {'post':post})
 
-def comments(request,id):
-    post = Post.objects.get(id=id)
-    comments = post.comments.all()
-    new_comment = None
-    if request.method == 'POST':
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            comment_form.instance.author = request.user
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.save()
-            comment_form  = CommentForm()
-
-    else:
-        comment_form = CommentForm()
-    context = {
-        'form': comment_form,
-        'post': post,
-        'comments': comments
-
-    }
-    return render(request,'blog_posts/comments.html',context)
 
 def update_comment(request,id):
     comment = Comment.objects.get(id=id)
     form = UpdateCommentForm(request.POST or None, instance = comment)
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect("/")
+
+        # redirects to the main post been commented on
+        return redirect(f'/posts/{comment.post.id}') 
     context= {'form':form}
     return render(request, 'blog_posts/update_comment.html', context)
 
 def delete_comment(request, id):
-    comment = Comment.objects.get(id=id)
+
+    # Gets the id of the selected comment
+    comment = Comment.objects.get(id=id) 
     if request.method == 'POST':
         comment.delete()
-        return HttpResponseRedirect('/')
+        # redirects to the main post been commented on
+        return redirect(f'/posts/{comment.post.id}')
     context = {'comment':comment}
     return render(request, 'blog_posts/delete_comment.html', context)
